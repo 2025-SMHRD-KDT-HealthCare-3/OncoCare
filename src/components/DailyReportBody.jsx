98cd820833e8d007d337704e87e0a61ace3fe893
송정인 <jungin9607@gmail.com>
2026-03-25 17:18:43 +0900

    데일리 배변기록, 컨디션 기록 틀 제작

--- src/components/DailyReportBody.jsx
+++ src/components/DailyReportBody.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import DailyDiet from './DailyDiet'
+import DailyBowel from './DailyBowel'
+import DailyCondition from './DailyCondition'
 
 const DailyReportBody = () => {
   return (
     <div className='main-content'>
         <DailyDiet></DailyDiet>
+        <DailyBowel></DailyBowel>
+        <DailyCondition></DailyCondition>
     </div>
   )
 }
