51be1dfc9c716cf7227037263e6a6c4e6dc9446c
HS <lhs488548@gmail.com>
2026-04-01 11:12:11 +0900

    수정

--- node/src/features/index.js
+++ node/src/features/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-const mainRouter = require('./main/main');
 const userRouter = require('./user/user');
 const recipeRouter = require('./recipe/recipe');
 const authRouter = require('./auth/auth');
@@ -10,9 +9,8 @@ const reportRouter = require('./reports/reports');
 const ingredientRouter = require('./ingredient/ingredient');
 const bowelRouter = require('./bowel/bowel');
 const conditionRouter = require('./condition/condition');
-// const llmRouter = require('./llm/llm');
+const llmRouter = require('./llm/llm');
 
-router.use('/main', mainRouter);
 router.use('/user', userRouter);
 router.use('/auth', authRouter);
 router.use('/recipe', recipeRouter);
@@ -21,6 +19,6 @@ router.use('/report', reportRouter);
 router.use('/ingredient', ingredientRouter);
 router.use('/bowel', bowelRouter);
 router.use('/condition', conditionRouter);
-// router.use('/ai', llmRouter);
+router.use('/ai', llmRouter);
 
 module.exports = router;
